e74879a02afedb074148509263c1ce0fcc0fcfb2
CesarCr300 <cc30122003@gmail.com>
2021-09-15 09:22:05 -0500

    funcion innecesaria borrada

--- data/modelos.js
+++ data/modelos.js
@@ -40,18 +40,7 @@ const productoEsquema = mongoose.Schema({
         type: clienteEsquema,
     },
 });
-const agregarProducto = async function(ruc, datos) {
-    const cliente = await Cliente.findById(ruc);
-    const producto = new Producto({
-        nombre: datos.nombre,
-        precio: datos.precio,
-        categoria: datos.categoria,
-        cliente: cliente,
-    });
-    producto.save();
-};
 const Cliente = mongoose.model("Cliente", clienteEsquema);
 const Producto = mongoose.model("Producto", productoEsquema);
 module.exports.Cliente = Cliente;
 module.exports.Producto = Producto;
-module.exports.agregarProducto = agregarProducto;

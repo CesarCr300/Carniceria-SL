d3deb3505a45153f5074b079f3ebc66fa56c29c3
CesarCr300 <cc30122003@gmail.com>
2021-09-19 21:19:07 -0500

    corregido-productos-noborrados

--- data/insercion-datos.js
+++ data/insercion-datos.js
@@ -6,36 +6,6 @@ require("dotenv").config();
 
 mongoose.connect(process.env.CONECCION_BD);
 
-const Petros = async function() {
-    try {
-        const cliente = await new Cliente({
-            nombre: "Petros",
-            _id: "20210799",
-        });
-        await cliente.save();
-        console.log(cliente);
-    } catch (e) {
-        console.log(e);
-    }
-};
-
-const agregarProductos = async() => {
-    try {
-        const producto = await new Producto({
-            nombre: "bisteck",
-            categoria: "res",
-            precio: 27.9,
-        });
-        const cliente = await Cliente.findById("20210799");
-        await cliente.productos.push(producto);
-        await producto.save();
-        await cliente.save();
-    } catch (e) {
-        console.log(e);
-    }
-};
-// agregarProductos();
-
 const mostrarProductos = async() => {
     try {
         const cliente = await Cliente.find().populate("productos");
@@ -45,12 +15,4 @@ const mostrarProductos = async() => {
     }
 };
 
-const actualizarProducto = async() => {
-    try {
-        const producto = await Producto.findByIdAndUpdate(
-            "6147de05813a66edd83aca01", { precio: 39.9 }
-        );
-    } catch (e) {}
-};
-// actualizarProducto();
 mostrarProductos();

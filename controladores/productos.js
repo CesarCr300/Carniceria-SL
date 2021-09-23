dcf252148c84324174e10e1938b91ada93958202
CesarCr300 <cc30122003@gmail.com>
2021-09-23 18:37:54 -0500

    productos-reestructurados

---
+++ controladores/productos.js
@@ -0,0 +1,58 @@
+const { Cliente, Producto } = require("../data/modelos");
+let categorias = ["res", "cerdo", "pollo", "carnero", "pavo", "otros"];
+
+module.exports.renderizarIndex = async(req, res) => {
+    const { id } = req.params;
+    const cliente = await Cliente.findById(id).populate("productos");
+    res.render("clientes/productos/mostrar", {
+        cliente,
+        productos: cliente.productos,
+    });
+}
+
+module.exports.renderizarNuevo = async(req, res) => {
+    const { id } = req.params;
+    res.render("clientes/productos/nuevo", { id });
+};
+
+module.exports.nuevo = async(req, res) => {
+    const { id } = req.params;
+    const { nombre, precio, categoria } = req.body;
+    const cliente = await Cliente.findById(id);
+    const producto = await Producto.create({
+        nombre,
+        precio,
+        categoria,
+    });
+    await producto.save();
+    cliente.productos.push(producto);
+    await cliente.save();
+    req.flash("exito", `${nombre} fue agregado exitosamente`)
+    res.redirect(`/clientes/${id}/productos`);
+};
+
+module.exports.renderizarEditar = async(req, res) => {
+    const { id, idProducto } = req.params;
+    const producto = await Producto.findById(idProducto);
+    res.render("clientes/productos/editar", { id, producto, categorias });
+};
+
+module.exports.editar = async(req, res) => {
+    const { id, idProducto } = req.params;
+    const { nombre, precio, categoria } = req.body;
+    await Producto.findByIdAndUpdate(idProducto, {
+        nombre,
+        precio,
+        categoria,
+    });
+    req.flash("exito", `${nombre} fue actualizado correctamente`)
+    res.redirect(`/clientes/${id}/productos`);
+};
+
+module.exports.borrar = async(req, res) => {
+    const { idProducto, id } = req.params;
+    await Cliente.findByIdAndUpdate(id, { $pull: { productos: idProducto } });
+    const producto = await Producto.findByIdAndDelete(idProducto, { new: true });
+    req.flash("exito", `${producto.nombre} fue eliminado correctamente`)
+    res.redirect(`/clientes/${id}/productos`);
+}
--- routes/productos.routes.js
+++ routes/productos.routes.js
@@ -1,66 +1,20 @@
 const { Router } = require("express");
 const router = new Router({ mergeParams: true });
 
+const productos = require("../controladores/productos");
 const { inicioSesion, esModerador } = require("../utils/middlewares");
-const modelos = require("../data/modelos");
-const Cliente = modelos.Cliente;
-const Producto = modelos.Producto;
-let categorias = ["res", "cerdo", "pollo", "carnero", "pavo", "otros"];
 
 router.use(inicioSesion)
 
-router.get("", async(req, res) => {
-    const { id } = req.params;
-    const cliente = await Cliente.findById(id).populate("productos");
-    res.render("clientes/productos/mostrar", {
-        cliente,
-        productos: cliente.productos,
-    });
-});
+router.get("/", productos.renderizarIndex);
 
-router.get("/nuevo", esModerador, async(req, res) => {
-    const { id } = req.params;
-    res.render("clientes/productos/nuevo", { id });
-});
-router.post("/", esModerador, async(req, res) => {
-    const { id } = req.params;
-    const { nombre, precio, categoria } = req.body;
-    const cliente = await Cliente.findById(id);
-    const producto = await Producto.create({
-        nombre,
-        precio,
-        categoria,
-    });
-    await producto.save();
-    cliente.productos.push(producto);
-    await cliente.save();
-    req.flash("exito", `${nombre} fue agregado exitosamente`)
-    res.redirect(`/clientes/${id}/productos`);
-});
+router.get("/nuevo", esModerador, productos.renderizarNuevo);
 
-router.get("/:idProducto/editar", esModerador, async(req, res) => {
-    const { id, idProducto } = req.params;
-    const producto = await Producto.findById(idProducto);
-    res.render("clientes/productos/editar", { id, producto, categorias });
-});
-router.put("/:idProducto", esModerador, async(req, res) => {
-    const { id, idProducto } = req.params;
-    const { nombre, precio, categoria } = req.body;
-    await Producto.findByIdAndUpdate(idProducto, {
-        nombre,
-        precio,
-        categoria,
-    });
-    req.flash("exito", `${nombre} fue actualizado correctamente`)
-    res.redirect(`/clientes/${id}/productos`);
-});
+router.post("/", esModerador, productos.nuevo);
 
-router.delete("/:idProducto", esModerador, async(req, res) => {
-    const { idProducto, id } = req.params;
-    await Cliente.findByIdAndUpdate(id, { $pull: { productos: idProducto } });
-    const producto = await Producto.findByIdAndDelete(idProducto, { new: true });
-    req.flash("exito", `${producto.nombre} fue eliminado correctamente`)
-    res.redirect(`/clientes/${id}/productos`);
-});
+router.get("/:idProducto/editar", esModerador, productos.renderizarEditar);
+router.put("/:idProducto", esModerador, productos.editar);
+
+router.delete("/:idProducto", esModerador, productos.borrar);
 
 module.exports = router;

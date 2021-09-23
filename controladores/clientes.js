b968fb746c740786229d502289a1d6c2f5cf1888
CesarCr300 <cc30122003@gmail.com>
2021-09-23 18:16:39 -0500

    clientes reestructurados

---
+++ controladores/clientes.js
@@ -0,0 +1,48 @@
+const { Cliente } = require("../data/modelos");
+
+module.exports.renrerizarIndex = async(req, res, next) => {
+    const clientes = await Cliente.find();
+    res.render("clientes/index.ejs", { clientes });
+}
+
+module.exports.renderizarNuevo = (req, res) => {
+    res.render("clientes/nuevo.ejs");
+}
+
+module.exports.nuevo = async(req, res) => {
+    const { ruc, nombre, direccion, numero } = req.body;
+    const cliente = await new Cliente({
+        _id: ruc,
+        nombre,
+        direccion,
+        numero,
+    });
+    await cliente.save();
+    req.flash("exito", "Cliente creado exitosamente")
+    res.redirect("/clientes");
+}
+
+module.exports.borrar = async(req, res) => {
+    const { id } = req.params;
+    const cliente = await Cliente.findOneAndDelete({ _id: id });
+    req.flash("exito", `${cliente.nombre} fue borrado exitosamente`)
+    res.redirect("/clientes");
+}
+
+module.exports.renderizarEditar = async(req, res) => {
+    const { id } = req.params;
+    const cliente = await Cliente.findById(id);
+    res.render("clientes/editar.ejs", { cliente });
+}
+
+module.exports.editar = async(req, res) => {
+    const { id } = req.params;
+    const { nombre, direccion, numero } = req.body;
+    await Cliente.findByIdAndUpdate(id, {
+        nombre,
+        direccion,
+        numero,
+    });
+    req.flash("exito", `${nombre} actualizado exitosamente`)
+    res.redirect("/clientes");
+}
--- routes/cliente.routes.js
+++ routes/cliente.routes.js
@@ -5,7 +5,8 @@ const modelos = require("../data/modelos");
 const asyncError = require("../utils/asyncError");
 const ExpressError = require("../utils/expressError");
 const Cliente = modelos.Cliente;
-const { inicioSesion, esModerador } = require("../utils/middlewares")
+const { inicioSesion, esModerador } = require("../utils/middlewares");
+const clientes = require("../controladores/clientes");
 
 const { esquemaCliente } = require("../esquemasJoi/validaciones")
 const validacionCliente = (req, res, next) => {
@@ -19,49 +20,16 @@ const validacionCliente = (req, res, next) => {
 
 router.use(inicioSesion)
 
-router.get("", asyncError(async(req, res, next) => {
-    const clientes = await Cliente.find();
-    res.render("clientes/index.ejs", { clientes });
-}));
+router.route("/")
+    .get(asyncError(clientes.renrerizarIndex))
+    .post(esModerador, validacionCliente, asyncError(clientes.nuevo));
 
-router.get("/nuevo", esModerador, (req, res) => {
-    res.render("clientes/nuevo.ejs");
-});
-router.post("/", esModerador, validacionCliente, asyncError(async(req, res) => {
-    const { ruc, nombre, direccion, numero } = req.body;
-    const cliente = await new Cliente({
-        _id: ruc,
-        nombre,
-        direccion,
-        numero,
-    });
-    await cliente.save();
-    req.flash("exito", "Cliente creado exitosamente")
-    res.redirect("/clientes");
-}));
+router.get("/nuevo", esModerador, clientes.renderizarNuevo);
 
-router.delete("/:id", esModerador, asyncError(async(req, res) => {
-    const { id } = req.params;
-    const cliente = await Cliente.findOneAndDelete({ _id: id });
-    req.flash("exito", `${cliente.nombre} fue borrado exitosamente`)
-    res.redirect("/clientes");
-}));
+router.route("/:id")
+    .delete(esModerador, asyncError(clientes.borrar))
+    .put(esModerador, validacionCliente, asyncError(clientes.editar));
 
-router.get("/:id/editar", esModerador, asyncError(async(req, res) => {
-    const { id } = req.params;
-    const cliente = await Cliente.findById(id);
-    res.render("clientes/editar.ejs", { cliente });
-}));
-router.put("/:id", esModerador, validacionCliente, asyncError(async(req, res) => {
-    const { id } = req.params;
-    const { nombre, direccion, numero } = req.body;
-    await Cliente.findByIdAndUpdate(id, {
-        nombre,
-        direccion,
-        numero,
-    });
-    req.flash("exito", `${nombre} actualizado exitosamente`)
-    res.redirect("/clientes");
-}));
+router.get("/:id/editar", esModerador, asyncError(clientes.renderizarEditar));
 
 module.exports = router;

19ad965aea517a5e28c52f9efa04d5a9908338d8
CesarCr300 <cc30122003@gmail.com>
2021-09-24 08:17:41 -0500

    usuarios controladores agregados

---
+++ controladores/usuarios.js
@@ -0,0 +1,27 @@
+const { Usuario } = require("../data/modelos");
+
+module.exports.renderizarRegistrar = (req, res) => {
+    res.render("usuario/registrar.ejs");
+};
+
+module.exports.registrar = async(req, res, next) => {
+    const { correo, username, password, rango } = req.body;
+    const usuario = new Usuario({ correo, username, rango });
+    const usuarioFinal = await Usuario.register(usuario, password)
+    req.flash("exito", `${username} creado correctamente.`)
+    res.redirect("/")
+}
+
+module.exports.renderizarLogin = (req, res) => {
+    res.render("usuario/login.ejs")
+}
+module.exports.login = async(req, res, next) => {
+    req.flash("exito", "Sesión Iniciada correctamente")
+    res.redirect("/")
+}
+
+module.exports.logout = (req, res) => {
+    req.logOut();
+    req.flash("exito", "Sesión cerrada correctamente");
+    res.redirect("/")
+}
--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -6,30 +6,16 @@ const passport = require("passport");
 const asyncError = require("../utils/asyncError");
 const { Usuario } = require("../data/modelos")
 const { inicioSesion, esModerador } = require("../utils/middlewares");
+const usuarios = require("../controladores/usuarios");
 
+router.route("/registrar")
+    .get(inicioSesion, usuarios.renderizarRegistrar)
+    .post(inicioSesion, esModerador, asyncError(usuarios.registrar))
 
-router.get("/registrar", inicioSesion, (req, res) => {
-    res.render("usuario/registrar.ejs");
-})
-router.post("/registrar", inicioSesion, esModerador, asyncError(async(req, res, next) => {
-    const { correo, username, password, rango } = req.body;
-    const usuario = new Usuario({ correo, username, rango });
-    const usuarioFinal = await Usuario.register(usuario, password)
-    req.flash("exito", `${username} creado correctamente.`)
-    res.redirect("/")
-}))
+router.route("/login")
+    .get(usuarios.renderizarLogin)
+    .post(passport.authenticate("local", { failureFlash: true, failureRedirect: "/" }), asyncError(usuarios.login))
 
-router.get("/login", (req, res) => {
-    res.render("usuario/login.ejs")
-})
-router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/" }), asyncError(async(req, res, next) => {
-    req.flash("exito", "Sesión Iniciada correctamente")
-    res.redirect("/")
-}))
+router.get("/logout", inicioSesion, usuarios.logout);
 
-router.get("/logout", inicioSesion, (req, res) => {
-    req.logOut();
-    req.flash("exito", "Sesión cerrada correctamente");
-    res.redirect("/")
-})
 module.exports = router;

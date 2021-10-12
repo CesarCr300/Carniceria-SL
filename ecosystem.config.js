9b77b17e319021c6fe3d05d7a31bec7a87b30839
CesarCr300 <cc30122003@gmail.com>
2021-10-12 12:07:00 -0500

    src

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "app",
-        script: "./app.js",
+        script: "./src/app.js",
         instances: "max",
         env: {
             NODE_ENV: "development",

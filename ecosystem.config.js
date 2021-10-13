56d5f75c1549ba1f1fd82d891ae55c307f1c1e58
CesarCr300 <cc30122003@gmail.com>
2021-10-13 14:20:31 -0500

    execution fixed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "app",
-        script: "./src/app.js",
+        script: "./src/index.js",
         instances: "max",
         env: {
             NODE_ENV: "development",
